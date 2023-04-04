408ab52e18665c1b144e76024a5c15191f1cc86f
Herick Moreira <contato.herickmoreira@gmail.com>
2023-04-04 20:41:13 -0300

    exercícios 3.5

---
+++ fundamentos/secao3- introducao-javascript/dia5/exercicios.js
@@ -0,0 +1,42 @@
+ function imprimeIdade() {
+     for (let idade = 30; idade <= 40; idade += 1) {
+       console.log('Idade dentro do for:', idade)
+     }
+   }
+   imprimeIdade()
+
+ const pessoa = {
+     nome: 'Henri',
+     idade: 20
+}
+ pessoa.nome = 'Luna'
+ pessoa.idade = 19
+
+     console.log('Nome:', pessoa.nome);
+     console.log('Idade:', pessoa.idade); 
+
+ let favoriteFood = 'Lasanha';
+ favoriteFood = 'Hambúrguer';
+ console.log(favoriteFood);
+
+ const name = 'Adriana';
+ const lastName = 'Soares';
+ console.log(`Olá, ${name} ${lastName}!`);
+ function soma(a,b) {
+   let resultado = a + b;
+   return resultado;
+ }
+ let a = 3;
+ let b = 5;
+ console.log(`O resultado da soma de ${a} + ${b} é ${soma(a,b)}`);
+
+const hello = (nome) => `Olá, ${nome}!`
+ let nome = 'Ivan';
+ console.log(hello(nome));
+
+ const numeroAleatorio = () => Math.random()
+ console.log(numeroAleatorio());
+
+let speed = 90;
+const speedCar = (speed <= 120) ? 'Você está na velocidade permitida' : 'Você ultrapassou o limite de velocidade';
+console.log(speedCar);
