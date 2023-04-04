68a67883bdffd9161959ead4d6cd8ebcd03b14f6
Herick Moreira <contato.herickmoreira@gmail.com>
2023-04-03 23:59:26 -0300

    exercícios Objetos

---
+++ fundamentos/secao3- introducao-javascript/dia4/OBJETOS/exercicio.objetos3.js
@@ -0,0 +1,10 @@
+let student = {
+    name: 'Herick Moreira',
+    email: 'contato.herickmoreira@gmail.com',
+    cellphone: '(21) 990693660',
+    LinkedIn: 'https://www.linkedin.com/in/herick-moreira/'
+};
+
+student.GitHub = 'https://github.com/Herick2D';
+
+console.log(student);
